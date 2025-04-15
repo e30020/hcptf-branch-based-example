92e2ce20b08b2b2919aa57e20080407893781c60
TSUGUO ISHIKAWA <e30020@jp.ibm.com>
2025-04-15 17:29:29 +0900

    change module3

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "pet_example_one_id" {
-  value = module.random_pet.example_1.id
+  value = module.random_pet_id_1.id
 }
 
 output "pet_example_two_id" {
-  value = module.random_pet.example_2.id
+  value = module.random_pet_id_2.id
 }
 
 output "pet_example_three_id" {
-  value = module.random_pet.example_3.id
+  value = module.random_pet_id_3.id
 }
