2d44c895bfd64cc7fca49261493e590115b193d6
TSUGUO ISHIKAWA <e30020@jp.ibm.com>
2025-04-15 17:40:57 +0900

    v1.1.0

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 module "example-module" {
   source  = "app.terraform.io/hashicorp_org_tel_sec/example-module/random"
-  version = "1.0.0"
+  version = "1.1.0"
   
   pet_length    = 5
   pet_prefix    = "module"
